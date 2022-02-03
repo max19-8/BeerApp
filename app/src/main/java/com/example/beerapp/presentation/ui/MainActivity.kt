087585c13397389ea42549bad3172e73b304c477
max19-8 <kozyrev9797@mail.ru>
2022-02-03 11:55:36 +0400

    added splash screen

--- app/src/main/java/com/example/beerapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/beerapp/presentation/ui/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.beerapp
+package com.example.beerapp.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.beerapp.R
+import com.example.beerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
